501ce48d27cbe891945270a0b640837a4a4453eb
Sasanka5426 <sasanka5426@gmail.com>
2021-09-27 23:54:27 +0530

    Updated graphs

--- app.py
+++ app.py
@@ -9,20 +9,7 @@ model = pickle.load(open('model.pkl', 'rb'))
 #default page of our web-app
 @app.route('/')
 def home():
-    # data = [
-    #     ("01-01-2020", 1597),
-    #     ("02-01-2020", 1456),
-    #     ("03-01-2020", 1908),
-    #     ("04-01-2020", 896),
-    #     ("05-01-2020", 755),
-    #     ("06-01-2020", 453),
-    #     ("07-01-2020", 1100),
-    #     ("08-01-2020", 1235),
-    #     ("09-01-2020", 1478),
-    # ]
-
-    # labels = [row[0] for row in data]
-    # values = [row[1] for row in data]
+    
 
     return render_template('Classification.html')
 
